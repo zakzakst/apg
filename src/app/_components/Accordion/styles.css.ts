f75aead095b03415f79463e967e88246555d49b6
zakzakst <e1433104@gmai.com>
2024-12-26 23:51:12 +0900

    修正

---
+++ src/app/_components/Accordion/styles.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+
+const main = style({
+  background: "#f00",
+});
+
+const title = style({
+  background: "#0f0",
+});
+
+export const accordion = {
+  main,
+  title,
+};
