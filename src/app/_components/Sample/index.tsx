3e5955721fd27368fb2f679082cf55b7749e8f9f
zakzakst <e1433104@gmai.com>
2024-12-29 15:51:56 +0900

    修正

--- src/app/_components/Sample/index.tsx
+++ src/app/_components/Sample/index.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { Button } from "react-aria-components";
+// import { button as styles } from "./styles.css";
+// import { button as styles } from "@/app/_components/Sample/styles.css";
+// console.log(styles);
 
 const Sample = () => {
-  return <Button onPress={() => alert("Hello world!")}>Sample</Button>;
+  return (
+    // <Button onPress={() => alert("Hello world!")} className={styles.main}>
+    <Button onPress={() => alert("Hello world!")}>Sample</Button>
+  );
 };
 
 export default Sample;
---
+++ src/app/_components/Sample/styles.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+const main = style({
+  backgroundColor: "#000",
+});
+
+export const button = {
+  main,
+};
